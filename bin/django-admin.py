5c561e7730fba81ec5d8f41a4958262418fa5442
Graham Chapman <chapman.graham.randall@gmail.com>
2013-10-23 21:36:52 -0400

    Upload project source

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Graham/Dropbox/Cornell/CS4302/Project/CS4302/WineTourEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
